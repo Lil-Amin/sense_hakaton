58da34a50096b3915f5a1df7610b7368d212e452
Al-Amin Uzhakhov <al-amin.uzhakhov@raiffeisen.ru>
2024-02-29 05:51:52 +0300

    parsing added

---
+++ resume_filter/parsing.py
@@ -0,0 +1,31 @@
+import json
+
+from pydantic import TypeAdapter
+
+from resume_filter import settings
+from resume_filter.models import SortedCandidateSelection, CandidateSelection
+
+
+FILENAME: str = "case_2_data_for_members.json"
+FILENAME2: str = "case_2_reference_without_resume_sorted.json"
+
+file_path: str = f"{settings.DATA_DIR}/{FILENAME}"
+file_path2: str = f"{settings.DATA_DIR}/{FILENAME2}"
+
+with open(file_path, 'r') as file:
+    json_dict: dict = json.load(file)
+
+with open(file_path2, 'r') as file:
+    json_dict2: dict = json.load(file)
+
+sorted_candidate_selection: list[SortedCandidateSelection] = TypeAdapter(
+    list[SortedCandidateSelection]
+).validate_python(json_dict)
+
+candidate_selection: CandidateSelection = TypeAdapter(
+    CandidateSelection
+).validate_python(json_dict2)
+
+
+print(sorted_candidate_selection)
+print(candidate_selection)
