9f28786b5bfc2d04264c24fa9a37ad21df978eaf
Al-Amin Uzhakhov <al-amin.uzhakhov@raiffeisen.ru>
2024-02-29 15:44:31 +0300

    filter_resumes, filter_resumes_file api added

--- resume_filter/app.py
+++ resume_filter/app.py
@@ -1,9 +1,41 @@
-from litestar import Litestar, get
+import typing
+
+from litestar import Litestar, get, post, MediaType
+from litestar.datastructures import UploadFile
+from litestar.enums import RequestEncodingType
+from litestar.params import Body
+
+from resume_filter.models import CandidateSelection
+
+
+def model_inference(candidate_selection: CandidateSelection) -> list[str]:
+    # dummy inference
+    ids: list[str] = [resume.uuid for resume in candidate_selection.resumes]
+
+    return ids
 
 
 @get("/")
-def index() -> str:
-    return "Hello World!"
+def hello() -> str:
+    return "Hello!"
 
 
-app: Litestar = Litestar([index])
+@post("/filter_resumes", sync_to_thread=False)
+def filter_resumes(data: CandidateSelection) -> list[str]:
+    ids: list[str] = model_inference(data)
+
+    return ids
+
+
+@post("/filter_resumes_file", media_type=MediaType.JSON, sync_to_thread=False)
+def filter_resumes_file(
+    data: typing.Annotated[UploadFile, Body(media_type=RequestEncodingType.MULTI_PART)]
+) -> list[str]:
+    content: bytes = data.file.read()
+    candidate_selection: CandidateSelection = CandidateSelection.model_validate_json(content)
+    ids: list[str] = model_inference(candidate_selection)
+
+    return ids
+
+
+app: Litestar = Litestar([hello, filter_resumes, filter_resumes_file])
