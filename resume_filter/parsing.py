b6a5e56306c1c5d930f33636220e77dd5caf9f32
Dmitry <dmi3dmi3@mail.ru>
2024-02-29 12:39:31 +0300

    minor refactoring

--- resume_filter/parsing.py
+++ resume_filter/parsing.py
@@ -5,25 +5,26 @@ from pydantic import TypeAdapter
 from resume_filter import settings
 from resume_filter.models import CandidateSelection, SortedCandidateSelection
 
-
 FILENAME: str = "case_2_data_for_members.json"
 FILENAME2: str = "case_2_reference_without_resume_sorted.json"
 
-file_path: str = f"{settings.DATA_DIR}/{FILENAME}"
-file_path2: str = f"{settings.DATA_DIR}/{FILENAME2}"
 
-with open(file_path, 'r') as file:
-    json_dict: dict = json.load(file)
-
-with open(file_path2, 'r') as file:
-    json_dict2: dict = json.load(file)
-
-sorted_candidate_selection: list[SortedCandidateSelection] = TypeAdapter(
-    list[SortedCandidateSelection]
-).validate_python(json_dict)
-
-candidate_selection: CandidateSelection = TypeAdapter(CandidateSelection).validate_python(json_dict2)
+def load_train() -> list[SortedCandidateSelection]:
+    file_path: str = f"{settings.DATA_DIR}/{FILENAME}"
+    with open(file_path, "r") as f:
+        json_dict: dict = json.load(f)
+    return TypeAdapter(list[SortedCandidateSelection]).validate_python(json_dict)
 
 
-print(sorted_candidate_selection)
-print(candidate_selection)
+def load_test() -> CandidateSelection:
+    file_path: str = f"{settings.DATA_DIR}/{FILENAME2}"
+    with open(file_path, "r") as f:
+        json_dict: dict = json.load(f)
+    return TypeAdapter(CandidateSelection).validate_python(json_dict)
+
+
+if __name__ == "__main__":
+    sorted_candidate_selection: list[SortedCandidateSelection] = load_train()
+    candidate_selection: CandidateSelection = load_test()
+    print(sorted_candidate_selection)
+    print(candidate_selection)
